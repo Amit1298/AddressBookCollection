f7a0aef9ffa62a3aa2ac74525c533ed630ceec48
Amit <amitkr24031995@gmail.com>
2021-10-14 17:44:37 +0530

    Adding Address book Created Contact

---
+++ UC1createContact/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UC1createContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> Contact = new Dictionary<string, string>();
+            Contact.Add("FirstName", "Amit");
+            Contact.Add("LastName", "Kumar");
+            Contact.Add("Address", "DefenceColony");
+            Contact.Add("City", "Agra");
+            Contact.Add("State", "Uttar Pradesh");
+            Contact.Add("PhoneNumber", "9897139203");
+            Contact.Add("Email", "Amitkr2403@gmail.com");
+
+            
+        }
+    }
+}
